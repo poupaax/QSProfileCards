c6965f41281317bca7b5210edef0cc84a5aa0527
Vítor Dias <2130134@my.ipleiria.pt>
2017-05-26 15:18:38 +0100

    change site location

--- Tests/src/test/java/CucumberRunner.java
+++
@@ -1,7 +0,0 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", "html:reports/test-report"})
-public class CucumberRunner { }
--- Tests/src/test/java/StepsDefs.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by vitor on 05/05/2017.
- */
-public class StepsDefs {
-}
